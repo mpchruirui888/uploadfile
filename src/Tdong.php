21ee6692c08032f62f1fab14fc0a641f0e1a1212
天冬 <1549431732@qq.com>
2021-03-01 16:57:35 +0800

    <feta>测试

---
+++ src/Tdong.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Mpchruirui888;
+
+class Tdong
+{
+    public function __construct()
+    {
+
+    }
+
+    public function demo_1()
+    {
+        return "demo1";
+    }
+}
--- src/UploadFile.php
+++ src/UploadFile.php
@@ -3,7 +3,7 @@
  * 通用文件上传类
  * @author  guanguans <yzmguanguan@gmail.com>
  */
-namespace Guanguans;  // 注意命名空间与 composer.json 中的一致
+namespace Mpchruirui888;  // 注意命名空间与 composer.json 中的一致
 
 class UploadFile
 {
